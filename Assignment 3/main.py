19d0644928881a42a1c67a0c7b045c6f8ab481e1
Jefferson Pule Mendez <japuleme@uwaterloo.ca>
2021-11-01 06:10:00 -0400

    [Loss function grows]

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,4 +1,123 @@
-# Write your assignment here
+# Digits Classification
+import numpy as np
+import  json, argparse, sys, os
+
+import torch 
+import torch.nn as nn #layer types
+import torch.nn.functional as func #useful ML functions that are applied to the layers
+import torch.optim as optim
+
+import matplotlib.pyplot as plt
+
+sys.path.append('src')
+from Neural_Network import Net
+
+def run_demo(lr,epochs, model, xi,yi, time_step):
+    for param in model.parameters():
+        param.requires_grad = True
+    # Divide data into inputs and lables, but also into a 3000 test 
+    X, Y, T,  XYini = model.getdata(xi,yi, time_step) 
+
+    # Define an optimizer and the loss function
+    
+#    optimizer = optim.SGD(model.parameters(), lr)  # We use gradient descent 
+
+    # If you want you could try Adam
+    optimizer = optim.SGD(model.parameters(), lr, momentum=0.99)
+
+    # The loss function is the cross entropy.
+    loss= torch.nn.MSELoss(reduction= 'mean')     
+
+    #Lists that will contain the information for each epoch
+    loss_train= []
+    acc_train=[]
+#    loss_test= []
+#    acc_test=[]
+    num_epochs= int(epochs)
+
+    # Training loop
+    for epoch in range(1, num_epochs + 1):
+  
+        train_val, accuracy= model.backprop(X, Y , T , XYini, loss, epoch, optimizer)
+        loss_train.append(train_val)
+        acc_train.append(accuracy)        
+            
+#        test_val, test_acc= model.test(test_inputs, test_targets, loss, epoch)       
+#        loss_test.append(test_val)
+#        acc_test.append(test_acc)
+
+        if (epoch+1) % 2 == 0:
+            print('Epoch [{}/{}]'.format(epoch+1, num_epochs)+\
+                      '\tTraining Loss: {:.4f}'.format(train_val)+\
+                      '\tTraining Accuracy: {:.4f}'.format(accuracy))
+
+    print('Final training loss: {:.4f}'.format(loss_train[-1]))
+    print('Final training accuracy: {:.4f}'.format(acc_train[-1]))
+
+    return  loss_train, acc_train
+
+#def plot_results(training, test, type_of_graph):
+#    assert len(training)==len(test), 'Length mismatch between the curves'
+#    num_epochs= len(training)
+
+    # Plot saved in results folder
+#    plt.plot(range(num_epochs), training, label= "Training "+type_of_graph, color="blue")
+#    plt.plot(range(num_epochs), test, label= "Test "+type_of_graph, color= "green")
+#    plt.legend()
+#    plt.savefig(args.output + '/fig'+type_of_graph+'.pdf')
+#    plt.close()
+
 
 if __name__ == '__main__':
-    print('hello')
+    #Command line arguments
+    parser= argparse.ArgumentParser(description='Inputs')
+    
+    parser.add_argument("-json", default="data/arguments.json", help="input path to json file, default: data/arguments.json")
+    parser.add_argument("-v", default=1 ,help="verbosity, default:1")
+    parser.add_argument("-results", default="results" ,help="path to folder to save results, default: results")
+    parser.add_argument("-xfield",help="expression of the x-component of the vector field")
+    parser.add_argument("-yfield",help="expression of the y-component of the vector field")
+    parser.add_argument("-lb",help="lower bound for initial conditions")
+    parser.add_argument("-ub",help="upper bound for initial conditions")
+    parser.add_argument("-ntests",help="number of test trajectories to plot")
+ 
+    args = parser.parse_args()
+
+    #Load data
+    lb=float(args.lb)
+    up=float(args.ub)
+    dx=args.xfield
+    dy=args.xfield
+
+    f=open(args.json)
+    json_file=json.load(f)
+    
+    lr=json_file['learning rate']   
+    epochs=json_file['Epochs']    
+    n_hidden=json_file['n_hidden']  #number of hidden
+    time_step=json_file['time step']
+
+    #Initial conditions
+    xi=np.random.uniform(lb,up)
+    yi=np.random.uniform(lb,up)
+
+    # Define global functions
+    u="u"
+    v="v"
+    exec("{} = lambda x,y: {}".format(u,dx))
+    exec("{} = lambda x,y: {}".format(v,dy))
+
+    df_x= lambda x,y: u(x,y)
+    df_y= lambda x,y: v(x,y)
+
+    #Creat an instance of the model
+
+    model=Net(n_hidden,time_step, xi, yi, df_x, df_y)
+    
+    #  Run the model 
+    loss_train, acc_train=run_demo(lr,epochs, model, xi,yi, time_step)
+    model.reset()
+
+    #Plot and save
+#    plot_results(loss_train, loss_test, "loss")
+#    plot_results(acc_train, acc_test, "accuracy")
---
+++ Assignment 3/src/Neural_Network.py
@@ -0,0 +1,142 @@
+'Neural Network Differential equations'
+import numpy as np
+import torch 
+import torch.nn as nn #layer types
+import torch.nn.functional as func #useful ML functions that are applied to the layers
+import torch.optim as optim
+
+
+class Net(nn.Module):
+    '''
+    Neural network class.
+    Architecture:
+        Two fully-connected layers fc1 and fc2.
+        Two nonlinear activation functions sigmoid and softmax.
+    '''
+    def __init__(self, n_hidden, time_step, xi, yi, df_x, df_y):
+        super(Net, self).__init__()
+
+        self.fc1= nn.Linear(4, n_hidden)
+        self.fc2= nn.Linear(n_hidden, n_hidden)
+        self.fc3= nn.Linear(n_hidden, 2)
+        self.df_x= df_x
+        self.df_y= df_y
+        self.f_t= lambda t,x,y, xyini: torch.add(xyini,torch.multiply(t,self.forward(torch.cat((x,y,t), axis=1).float())))
+
+    # Feedforward function
+    def forward(self, inputs):
+        h = torch.sigmoid(self.fc1(inputs))
+        tanh = torch.nn.Tanh()
+        p=tanh(self.fc2(h))
+        y=self.fc3(p)
+        return y
+    
+    # Reset function for the training weights
+    # Use if the same network is trained multiple times.
+    def reset(self):
+        self.fc1.reset_parameters()
+        self.fc2.reset_parameters()
+        
+    # Backpropagation function
+    def backprop(self, X, Y, T, XYini, loss, epoch, optimizer):
+        self.train()
+        # Find the resulting derivatives and the actual derivatives
+        
+        results, targets=self.derivatives(T, X, Y, XYini, self.f_t, self.df_x, self.df_y, optimizer)
+        print("restults", results)
+        print("targets", targets)
+        # Calculate the accuracy of our results
+        acc=self.accuracy(results,targets)
+        
+        # Back propagate the error
+#        obj_val= loss(results, targets)
+        optimizer.zero_grad()
+        obj_val=torch.autograd.Variable(loss(results, targets),requires_grad=True)
+        obj_val.backward()         # Update the values 
+        optimizer.step()
+
+        return obj_val.item(), acc
+    
+    # Test function. Avoids calculation of gradients.
+ #   def test(self, inputs, targets, loss, epoch):
+ #       self.eval()
+
+        # Run the NN without calculating gradients
+ #       with torch.no_grad():
+ #           targets= torch.flatten(torch.from_numpy(targets))
+ #           results=self.forward(inputs)
+ #           acc=self.accuracy(results,targets)
+ #           cross_val= loss(results, (targets/2).type(torch.LongTensor))
+        
+ #           return cross_val.item(), acc
+
+    # Calculates the porcentage of values that were correctly labeled by our NN    
+    def accuracy(self, results, targets):
+
+        accuracy=torch.mean(torch.add(results,targets, alpha=-1))
+
+        return accuracy
+
+    # Splits the data into inputs and targets for the training and also generates a test set.
+    def getdata(self,xi,yi, time_step):
+
+        t=np.arange(0,1, time_step)
+
+        x=np.arange(-1,1,0.01)
+        y=np.arange(-1,1,0.01)        
+        x,y=np.meshgrid(y,x)
+        xy=np.column_stack((y.ravel(),x.ravel()))
+
+        inp=np.zeros((t.shape[0],xy.shape[0],4))
+        k=0
+        for i in t:
+            zeros=np.zeros(xy.shape)+i
+            inp[k]+=np.append(xy,zeros, axis=1)
+            k+=1
+        inp=np.reshape(inp, (inp.shape[0]*inp.shape[1],inp.shape[2]))
+
+        X=inp[:,0]
+        X=np.reshape(X,(X.shape[0],1))
+
+        Y=inp[:,1]
+        Y=np.reshape(Y,(Y.shape[0],1))
+
+        T=inp[:,2:4]
+        T=np.reshape(T,(T.shape[0],2))
+
+        Xini=np.full((X.shape),xi)
+        Yini=np.full((X.shape),yi)
+        XYini=np.concatenate((Xini,Yini), axis=1)
+
+        XYini=torch.tensor(XYini).float()
+        X=torch.tensor(X).float()
+        Y=torch.tensor(Y).float()
+        T=torch.tensor(T).float()
+  
+
+        return X, Y, T, XYini
+
+    
+    # Calculate the exact derivative and the nn derivative in function of time
+    def df(self ,x,y, df_x, df_y):
+        dx=df_x(x,y)
+        dy=df_y(x,y)
+        return torch.cat((dx,dy), axis=1)
+
+    def derivatives(self, T, X, Y, XYini, f_t, df_x, df_y, optimizer):
+        T.requires_grad=True         
+        outputs=f_t(T,X,Y, XYini)
+        out_x=torch.reshape(outputs[:,0].detach(),X.shape)
+        out_y=torch.reshape(outputs[:,1].detach(),Y.shape)
+    
+        outputs.backward(gradient=torch.ones(outputs.size()))
+
+        df_exp=self.df(out_x,out_y, df_x, df_y)
+        df_nn=T.grad
+        optimizer.zero_grad()
+        return  df_nn, df_exp
+        
+
+
+
+
